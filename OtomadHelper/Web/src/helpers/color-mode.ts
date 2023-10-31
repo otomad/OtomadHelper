8257df1c52499dd737b92802d6dcdeb7402e9351
otomad <shuishenme@outlook.com>
2023-10-31 23:15:31 +0800

    Better color mode

--- OtomadHelper/Web/src/helpers/color-mode.ts
+++ OtomadHelper/Web/src/helpers/color-mode.ts
@@ -1,14 +1,15 @@
 export type ColorScheme = "light" | "dark" | "auto";
 const lightModePreference = window.matchMedia("(prefers-color-scheme: light)");
-export const changeColorScheme = (isLight?: boolean | ColorScheme) => {
+const changeColorScheme = (isLight?: boolean | ColorScheme) => {
 	if (typeof isLight === "string")
 		isLight = isLight === "light" ? true : isLight === "dark" ? false : undefined;
 	if (isLight === undefined) isLight = lightModePreference.matches;
 	document.documentElement.dataset.scheme = isLight ? "light" : "dark";
 };
-const getUserColorScheme = () => (JSON.parse(localStorage.getItem("colorMode")!)?.state as ZustandState<typeof useColorMode>).scheme;
+const getUserColorScheme = () => useColorMode.getState().scheme;
 
 export function initColorMode() {
 	lightModePreference.addEventListener("change", e => getUserColorScheme() === "auto" && changeColorScheme(e.matches));
 	changeColorScheme(getUserColorScheme());
+	useColorMode.subscribe(({ scheme }) => changeColorScheme(scheme));
 }
--- OtomadHelper/Web/src/stores/color-mode.ts
+++ OtomadHelper/Web/src/stores/color-mode.ts
@@ -1,9 +1,9 @@
-import { changeColorScheme, type ColorScheme } from "helpers/color-mode";
+import type { ColorScheme } from "helpers/color-mode";
 
 export const useColorMode = createStore<{
 	scheme: ColorScheme;
 	setScheme: (scheme: ColorScheme) => void;
-}>()(persist((set, _get) => ({
+}>()(persist(set => ({
 	scheme: "auto",
-	setScheme: scheme => { set({ scheme }); changeColorScheme(scheme); },
+	setScheme: scheme => set({ scheme }),
 }), { name: "colorMode" }));
