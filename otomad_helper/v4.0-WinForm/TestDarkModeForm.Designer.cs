b659d3d56a2a80e1dbf7fb0824639403824e6d30
otomad <shuishenme@outlook.com>
2023-11-17 21:59:36 +0800

    Add a test

---
+++ otomad_helper/v4.0-WinForm/TestDarkModeForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Otomad.VegasScript.OtomadHelper.V4 {
+	partial class TestDarkModeForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.Windows.Forms.ListViewItem listViewItem13 = new System.Windows.Forms.ListViewItem("ListViewItem 1");
+			System.Windows.Forms.ListViewItem listViewItem14 = new System.Windows.Forms.ListViewItem("ListViewItem 2");
+			System.Windows.Forms.ListViewItem listViewItem15 = new System.Windows.Forms.ListViewItem("ListViewItem 3");
+			System.Windows.Forms.ListViewItem listViewItem16 = new System.Windows.Forms.ListViewItem("ListViewItem 4");
+			System.Windows.Forms.ListViewItem listViewItem17 = new System.Windows.Forms.ListViewItem("ListViewItem 5");
+			System.Windows.Forms.ListViewItem listViewItem18 = new System.Windows.Forms.ListViewItem("ListViewItem 6");
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Items.AddRange(new object[] {
+            "CheckListBoxItem 1",
+            "CheckListBoxItem 2",
+            "CheckListBoxItem 3",
+            "CheckListBoxItem 4",
+            "CheckListBoxItem 5",
+            "CheckListBoxItem 6"});
+			this.checkedListBox1.Location = new System.Drawing.Point(44, 23);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(671, 70);
+			this.checkedListBox1.TabIndex = 0;
+			// 
+			// hScrollBar1
+			// 
+			this.hScrollBar1.Location = new System.Drawing.Point(44, 113);
+			this.hScrollBar1.Name = "hScrollBar1";
+			this.hScrollBar1.Size = new System.Drawing.Size(671, 21);
+			this.hScrollBar1.TabIndex = 1;
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+			this.listView1.HideSelection = false;
+			this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem13,
+            listViewItem14,
+            listViewItem15,
+            listViewItem16,
+            listViewItem17,
+            listViewItem18});
+			this.listView1.Location = new System.Drawing.Point(44, 156);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(671, 145);
+			this.listView1.TabIndex = 2;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Width = 240;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Width = 240;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Width = 240;
+			// 
+			// vScrollBar1
+			// 
+			this.vScrollBar1.Location = new System.Drawing.Point(739, 23);
+			this.vScrollBar1.Name = "vScrollBar1";
+			this.vScrollBar1.Size = new System.Drawing.Size(21, 447);
+			this.vScrollBar1.TabIndex = 3;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+			this.dataGridView1.Location = new System.Drawing.Point(44, 320);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 27;
+			this.dataGridView1.Size = new System.Drawing.Size(671, 150);
+			this.dataGridView1.TabIndex = 4;
+			// 
+			// Column1
+			// 
+			this.Column1.HeaderText = "Column1";
+			this.Column1.MinimumWidth = 6;
+			this.Column1.Name = "Column1";
+			this.Column1.Width = 240;
+			// 
+			// Column2
+			// 
+			this.Column2.HeaderText = "Column2";
+			this.Column2.MinimumWidth = 6;
+			this.Column2.Name = "Column2";
+			this.Column2.Width = 240;
+			// 
+			// Column3
+			// 
+			this.Column3.HeaderText = "Column3";
+			this.Column3.MinimumWidth = 6;
+			this.Column3.Name = "Column3";
+			this.Column3.Width = 240;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(44, 490);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(101, 24);
+			this.checkBox1.TabIndex = 5;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(171, 490);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(117, 24);
+			this.radioButton1.TabIndex = 6;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "radioButton1";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(650, 485);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(110, 33);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// TestDarkModeForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.ClientSize = new System.Drawing.Size(794, 535);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.vScrollBar1);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.hScrollBar1);
+			this.Controls.Add(this.checkedListBox1);
+			this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+			this.Name = "TestDarkModeForm";
+			this.Text = "TestDarkModeForm";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private System.Windows.Forms.HScrollBar hScrollBar1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.VScrollBar vScrollBar1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ otomad_helper/v4.0-WinForm/TestDarkModeForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Otomad.VegasScript.OtomadHelper.V4 {
+	public partial class TestDarkModeForm : Form {
+		public TestDarkModeForm() {
+			InitializeComponent();
+			for (int i = 0; i < 6; i++) {
+				int index = dataGridView1.Rows.Add();
+				dataGridView1.Rows[index].Cells[0].Value = "Cell " + index;
+			}
+			ThemeAllControls();
+		}
+
+		private void ThemeAllControls(Control parent = null) {
+			parent = parent ?? this;
+			Action<Control> Theme = control => {
+				int trueValue = 0x01;
+				SetWindowTheme(control.Handle, "DarkMode_Explorer", null);
+				DwmSetWindowAttribute(control.Handle, DwmWindowAttribute.DWMWA_USE_IMMERSIVE_DARK_MODE, ref trueValue, Marshal.SizeOf(typeof(int)));
+				DwmSetWindowAttribute(control.Handle, DwmWindowAttribute.DWMWA_MICA_EFFECT, ref trueValue, Marshal.SizeOf(typeof(int)));
+				control.BackColor = Color.FromArgb(32, 32, 32);
+				control.ForeColor = Color.White;
+			};
+			if (parent == this) Theme(this);
+			foreach (Control control in parent.Controls) {
+				Theme(control);
+				if (control.Controls.Count != 0)
+					ThemeAllControls(control);
+			}
+		}
+
+		[DllImport("uxtheme.dll", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+		public static extern int SetWindowTheme(IntPtr hWnd, string pszSubAppName, string pszSubIdList);
+		[DllImport("dwmapi.dll")]
+		public static extern int DwmSetWindowAttribute(IntPtr hwnd, DwmWindowAttribute dwAttribute, ref int pvAttribute, int cbAttribute);
+		[Flags]
+		public enum DwmWindowAttribute : uint {
+			DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
+			DWMWA_MICA_EFFECT = 1029
+		}
+	}
+}
