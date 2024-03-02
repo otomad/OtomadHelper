5a19336faea302000c298b7e0cea898ca31e0c9e
otomad <shuishenme@outlook.com>
2024-03-02 21:27:57 +0800

    Update when initial change color scheme

--- OtomadHelper/Web/src/helpers/color-mode.ts
+++ OtomadHelper/Web/src/helpers/color-mode.ts
@@ -11,29 +11,24 @@ const changeColorScheme = (isLight?: boolean | ColorScheme) => {
 	const updateThemeSettings = () => document.documentElement.dataset.scheme = isLight ? "light" : "dark";
 	const afterUpdateThemeSettings = () => useColorModeStore.setState(() => ({ backgroundColor: getComputedStyle(document.body).backgroundColor }));
 
-	if (!lastClickMouseEvent) {
-		updateThemeSettings();
-		setTimeout(afterUpdateThemeSettings, 1000);
-	} else {
-		const { x, y } = lastClickMouseEvent;
-		const endRadius = Math.hypot(Math.max(x, window.innerWidth - x), Math.max(y, window.innerHeight - y));
-		const clipPath = [
-			`circle(0px at ${x}px ${y}px)`,
-			`circle(${endRadius}px at ${x}px ${y}px)`,
-		];
-		const CHANGING_COLOR_SCHEME_CLASS = "changing-color-scheme";
-		document.documentElement.classList.add(CHANGING_COLOR_SCHEME_CLASS);
-		startViewTransition(updateThemeSettings, {
-			clipPath: isLight ? clipPath : clipPath.toReversed(),
-		}, {
-			duration: 300,
-			easing: eases.easeInOutSmooth,
-			pseudoElement: isLight ? "::view-transition-new(root)" : "::view-transition-old(root)",
-		}).then(() => {
-			document.documentElement.classList.remove(CHANGING_COLOR_SCHEME_CLASS);
-			afterUpdateThemeSettings();
-		});
-	}
+	const { x, y } = lastClickMouseEvent ?? { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+	const endRadius = Math.hypot(Math.max(x, window.innerWidth - x), Math.max(y, window.innerHeight - y));
+	const clipPath = [
+		`circle(0px at ${x}px ${y}px)`,
+		`circle(${endRadius}px at ${x}px ${y}px)`,
+	];
+	const CHANGING_COLOR_SCHEME_CLASS = "changing-color-scheme";
+	document.documentElement.classList.add(CHANGING_COLOR_SCHEME_CLASS);
+	startViewTransition(updateThemeSettings, {
+		clipPath: isLight ? clipPath : clipPath.toReversed(),
+	}, {
+		duration: 300,
+		easing: eases.easeInOutSmooth,
+		pseudoElement: isLight ? "::view-transition-new(root)" : "::view-transition-old(root)",
+	}).then(() => {
+		document.documentElement.classList.remove(CHANGING_COLOR_SCHEME_CLASS);
+		afterUpdateThemeSettings();
+	});
 };
 const getUserColorScheme = () => useColorModeStore.getState().scheme;
 
