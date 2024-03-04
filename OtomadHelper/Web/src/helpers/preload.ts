fef5132bbda99bedb34c0d6ccea49abbeeab29d8
otomad <shuishenme@outlook.com>
2024-03-05 02:49:50 +0800

    Update dist

--- OtomadHelper/Web/src/helpers/preload.ts
+++ OtomadHelper/Web/src/helpers/preload.ts
@@ -1,8 +1,6 @@
-const images = import.meta.glob<string>("/src/assets/images/**/*", { import: "default" });
-for (const fetchImage of Object.values(images))
-	fetchImage().then(imagePath => {
-		try {
-			const image = new Image();
-			image.src = imagePath;
-		} catch { }
-	});
+const images = import.meta.glob<string>("/src/assets/images/**/*", { import: "default", eager: true });
+for (const imagePath of Object.values(images))
+	try {
+		const image = new Image();
+		image.src = imagePath;
+	} catch { }
