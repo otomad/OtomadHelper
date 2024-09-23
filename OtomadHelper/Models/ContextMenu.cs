6b49211d20f85931535cf57f7bed9a2221b4c84a
Rantetsu Inori <56647156+otomad@users.noreply.github.com>
2024-09-23 19:41:26 +0800

    chore: rename

--- OtomadHelper/Models/ContextMenu.cs
+++ OtomadHelper/Models/ContextMenu.cs
@@ -2,16 +2,16 @@ using Microsoft.Web.WebView2.Core;
 
 namespace OtomadHelper.Models;
 
-internal struct ContextMenu {
-	public string uuid;
-	public ContextMenuItem[] items;
+internal struct ContextMenu() {
+	public string uuid = "";
+	public ContextMenuItem[] items = [];
 }
 
-internal struct ContextMenuItem {
-	public CoreWebView2ContextMenuItemKind kind;
-	public string label;
-	public string uuid;
-	public bool @checked;
-	public bool enabled;
-	public ContextMenuItem[]? items;
+internal struct ContextMenuItem() {
+	public CoreWebView2ContextMenuItemKind kind = CoreWebView2ContextMenuItemKind.Command;
+	public string label = "";
+	public string uuid = "";
+	public bool @checked = false;
+	public bool enabled = true;
+	public ContextMenuItem[]? items = null;
 }
