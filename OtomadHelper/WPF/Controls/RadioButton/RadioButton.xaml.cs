0ec570bb7dc8c43eabfa0eaff9db05221cad9e9a
Rantetsu Inori <56647156+otomad@users.noreply.github.com>
2024-09-30 22:34:36 +0800

    fix(radio button): make focus style only on keyboard focus

--- OtomadHelper/WPF/Controls/RadioButton/RadioButton.xaml.cs
+++ OtomadHelper/WPF/Controls/RadioButton/RadioButton.xaml.cs
@@ -7,9 +7,11 @@ namespace OtomadHelper.WPF.Controls;
 public partial class RadioButtonResourceDictionary : CustomControlResourceDictionary {
 	private bool IsPressed { get; set; } = false;
 	private void UpdateStates_Hover(object sender, MouseEventArgs e) => UpdateStates((RadioButton)sender);
-	private void UpdateStates_Press(object sender, MouseButtonEventArgs e) { IsPressed = true; UpdateStates((RadioButton)sender); }
-	private void UpdateStates_Lift(object sender, MouseButtonEventArgs e) { IsPressed = false; UpdateStates((RadioButton)sender); }
+	private void UpdateStates_Press(object sender, MouseButtonEventArgs? e) { IsPressed = true; UpdateStates((RadioButton)sender); }
+	private void UpdateStates_Lift(object sender, MouseButtonEventArgs? e) { IsPressed = false; UpdateStates((RadioButton)sender); }
 	private void UpdateStates_Check(object sender, RoutedEventArgs e) => UpdateStates((RadioButton)sender);
+	private void UpdateStates_KeyDown(object sender, KeyEventArgs e) { if (e.Key == Key.Space) UpdateStates_Press(sender, null); }
+	private void UpdateStates_KeyUp(object sender, KeyEventArgs e) { if (e.Key == Key.Space) UpdateStates_Lift(sender, null); }
 
 	private void UpdateStates(RadioButton radioButton) {
 		string @checked = radioButton.IsChecked == true ? "Checked" : "Unchecked";
