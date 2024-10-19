0ac86c1c89b9aef66830ad0ca7e52f9f6dba5b65
Rantetsu Inori <56647156+otomad@users.noreply.github.com>
2024-10-20 03:55:16 +0800

    chore: update main.tsx

--- OtomadHelper/Web/src/main.tsx
+++ OtomadHelper/Web/src/main.tsx
@@ -1,4 +1,4 @@
-import { ClickToComponent } from "click-to-react-component";
+// import { ClickToComponent } from "click-to-react-component";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -6,8 +6,8 @@ import "./initial";
 import "./locales/config";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	// <React.StrictMode>
-	<App />,
-	// 	<ClickToComponent />
-	// </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+		{/* <ClickToComponent /> */}
+	</React.StrictMode>,
 );
