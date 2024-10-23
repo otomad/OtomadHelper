2b8b4c7f50aa25d0fc8e8827b2c35aa1a7c0a5e3
Rantetsu Inori <56647156+otomad@users.noreply.github.com>
2024-10-24 03:52:19 +0800

    feat(shupelunker): update piano keyboard colors

--- OtomadHelper/Web/src/components/Preview/PreviewPiano.tsx
+++ OtomadHelper/Web/src/components/Preview/PreviewPiano.tsx
@@ -7,6 +7,16 @@ const BLACK_KEY_WIDTH = 60;
 const Wrapper = styled.div`
 	overflow-x: auto;
 	border-radius: 6px;
+
+	> * {
+		border-radius: inherit;
+
+		> :first-child,
+		> :first-child > :first-child {
+			border-start-start-radius: inherit;
+			border-end-start-radius: inherit;
+		}
+	}
 `;
 
 const StyledPreviewPiano = styled.div`
@@ -34,41 +44,53 @@ export default function PreviewPiano({ ...htmlAttrs }: FCP<{}, "div">) {
 	);
 }
 
-const textColors = colors["foreground-color"];
-const fills = colors["fill-color-control-solid-default"];
-const fillsHover = colors["fill-color-control-solid-secondary"];
-const fillsPressed = colors["fill-color-control-solid-tertiary"];
-const strokes = colors["stroke-color-control-stroke-default"];
+const fills = ["rgba(255, 255, 255, 0.7)", "rgba(69, 69, 69)"];
+const fillsDark = ["rgba(255, 255, 255, 0.1)", "rgb(32, 32, 32)"] as const;
+const strokes = colors["stroke-color-card-stroke-default"];
+const strokeDarkWhite = "rgba(0, 0, 0, 0.12)";
 
 const StyledPianoKey = styled.div`
 	${styles.mixins.square("100%")};
 	position: relative;
 
 	button {
+		--highlight: transparent;
+		--level-highlight: 0%;
 		display: flex;
 		justify-content: center;
 		align-items: flex-end;
 		contain: strict;
+		color: ${c("foreground-color")};
 		font-variant-numeric: tabular-nums;
+		background-color: color-mix(in srgb, var(--fill), var(--highlight) var(--level-highlight));
 		border: 1px solid;
+
+		&:hover {
+			background-color: color-mix(in srgb, color-mix(in hsl, var(--fill), var(--mixed-hover) var(--level-hover, 10%)), var(--highlight) var(--level-highlight));
+		}
+
+		&:active {
+			background-color: color-mix(in srgb, color-mix(in hsl, var(--fill), var(--mixed-active, var(--mixed-hover)) var(--level-active, 20%)), var(--highlight) var(--level-highlight));
+		}
 	}
 
 	.white {
 		${styles.mixins.square("100%")};
+		--fill: ${fills[0]};
+		--mixed-hover: black;
+		--level-hover: 6.5%;
+		--level-active: 13%;
 		position: relative;
 		z-index: 1;
 		padding-bottom: 12px; // Avoid floating scrollbar taking up position.
 		min-inline-size: 24px;
-		color: ${textColors[0]};
-		background-color: ${fills[0]};
 		border-color: ${strokes[0]};
 
-		&:hover {
-			background-color: ${fillsHover[0]};
-		}
-
-		&:active {
-			background-color: ${fillsPressed[0]};
+		${ifColorScheme.dark} & {
+			--fill: ${fillsDark[0]};
+			--mixed-hover: white;
+			--level-active: 11%;
+			border-color: ${strokeDarkWhite};
 		}
 
 		&:focus-visible {
@@ -78,37 +100,51 @@ const StyledPianoKey = styled.div`
 
 	.black {
 		${styles.mixins.square("100%")};
+		--fill: ${fills[1]};
+		--mixed-hover: white;
+		--level-hover: 12%;
+		--level-active: 24%;
 		position: absolute;
 		top: 0;
 		right: ${-BLACK_KEY_WIDTH / 2}%;
 		z-index: 3;
 		width: ${BLACK_KEY_WIDTH}%;
 		height: 50%;
-		color: ${textColors[1]};
-		background-color: ${fills[1]};
 		border-color: ${strokes[1]};
 		border-radius: 0 0 4px 4px;
 
-		&:hover {
-			background-color: ${fillsHover[1]};
-		}
-
-		&:active {
-			background-color: ${fillsPressed[1]};
+		${ifColorScheme.dark} & {
+			--fill: ${fillsDark[1]};
+			--level-hover: 6%;
+			--level-active: 9%;
 		}
 	}
 
 	.active {
+		--fill: ${c("colorization")} !important;
+		--mixed-hover: white;
+		--mixed-active: black;
+		--level-hover: 10%;
+		--level-active: 10%;
 		color: ${getClearColorFromBackgroundColor("colorization")};
-		background-color: ${c("colorization")};
+	}
 
-		&:hover {
-			background-color: ${c("colorization", 90)};
-		}
+	&:not(:last-child, :hover, :has(> .white.active)) > .white {
+		border-inline-end-color: transparent;
+	}
 
-		&:active {
-			background-color: ${c("colorization", 80)};
-		}
+	&:has(> .white:is(:hover, .active)) + * > .white {
+		border-inline-start-color: transparent;
+	}
+
+	.source:not(.active) {
+		--highlight: ${c("colorization")};
+		--level-highlight: 15%;
+	}
+
+	.default:not(.source, .active) {
+		--highlight: ${c("fill-color-system-caution")};
+		--level-highlight: 15%;
 	}
 `;
 
@@ -132,10 +168,14 @@ function PianoKey({ isBlackNext, midiNote, showCOnly, activeKey: [activeKey, set
 		if (!isNoteNameC(blackSpn)) blackSpnShown = "";
 	}
 
+	// FOR TEST ONLY!
+	const hasSource = whiteSpn.endsWith("5");
+	const hasDefault = whiteSpn.endsWith("3");
+
 	return (
 		<StyledPianoKey>
-			<button type="button" className={["white", { active: activeKey === whiteSpn }]} onClick={() => setActiveKey?.(whiteSpn)}>{whiteSpnShown}</button>
-			{isBlackNext && <button type="button" className={["black", { active: activeKey === blackSpn }]} onClick={() => setActiveKey?.(blackSpn)}>{blackSpnShown}</button>}
+			<button type="button" className={["white", { active: activeKey === whiteSpn, source: hasSource, default: hasDefault }]} onClick={() => setActiveKey?.(whiteSpn)}>{whiteSpnShown}</button>
+			{isBlackNext && <button type="button" className={["black", { active: activeKey === blackSpn, source: hasSource, default: hasDefault }]} onClick={() => setActiveKey?.(blackSpn)}>{blackSpnShown}</button>}
 		</StyledPianoKey>
 	);
 }
