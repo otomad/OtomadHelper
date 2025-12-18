daa811f6064c87ab6d763d650b28c0036224a72f
Rantetsu Inori <shuishenme@outlook.com>
2025-12-19 03:44:34 +0800

    feat: update

--- preview/assets/dpi.BDO2sfK9.js
+++
@@ -1,1 +0,0 @@
-(function(){"use strict";let t=null;const e=()=>{t?.();const n=`(resolution: ${window.devicePixelRatio}dppx)`,i=window.matchMedia(n);i.addEventListener("change",e),t=()=>i.removeEventListener("change",e),document.documentElement.style.setProperty("--dpi",String(window.devicePixelRatio))};e()})();
