054fa015357b9e70d28914b3ba39a75c2461e7f1
Rantetsu Inori <shuishenme@outlook.com>
2026-01-08 00:44:26 +0800

    refactor: forget to update jsdoc

--- OtomadHelper/Web/src/hooks/state.ts
+++ OtomadHelper/Web/src/hooks/state.ts
@@ -30,6 +30,17 @@ type StatePropertyTuple<Tuple extends [...Any[]]> = {
 	[Index in keyof Tuple]: StatePropertyNonNull<Tuple[Index]>;
 } & { length: Tuple["length"] };
 
+/**
+ * Creates an array of state hooks from initial values.
+ * @template T - A tuple type extending an array of any values.
+ * @param initialValues - The initial values for each state hook.
+ * @returns A tuple of state hooks corresponding to the initial values.
+ * @example
+ * ```javascript
+ * const [[name, setName], [age, setAge]] = useStateList("John", 30);
+ * const [expanded1, expanded2, expanded3] = useStateList(true, true, true);
+ * ```
+ */
 export function useStateList<T extends Any[]>(...initialValues: T) {
 	return Array.from({ length: initialValues.length }, (_, i) => useState(initialValues[i])) as StatePropertyTuple<T>;
 }
