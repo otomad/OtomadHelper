034a34e5bd32c2d13fd16d6d33fa62ab80c61759
Rantetsu Inori <shuishenme@outlook.com>
2026-01-25 07:34:48 +0800

    feat(attached): finish new spacing logic

--- OtomadHelper/WPF/Common/Attached/Spacing.cs
+++ OtomadHelper/WPF/Common/Attached/Spacing.cs
@@ -52,18 +52,40 @@ public partial class Spacing {
 		double horizontal = GetHorizontal(panel), vertical = GetVertical(panel);
 		int childCount = panel.Children.Count;
 
-		// Go over the children and set margin for them:
+		// Memorize the uniform grid columns and rows, do not need to recalculate them each time when traversing.
+		(int columns, int rows)? uniformGridSize = null;
+		(int columns, int rows) GetUniformGridSize(UniformGrid uniformGrid) {
+			if (uniformGridSize.HasValue) return uniformGridSize.Value;
+			int columns = uniformGrid.Columns, rows = uniformGrid.Rows;
+			if (columns == 0 && rows == 0)
+				columns = rows = (int)Math.Ceiling(Math.Sqrt(childCount));
+			else if (rows == 0)
+				rows = (int)Math.Ceiling((double)childCount / columns);
+			else if (columns == 0)
+				columns = (int)Math.Ceiling((double)childCount / rows);
+			return (columns, rows);
+		}
+
+		// Go over the children and set margin for them.
 		for (int i = 0; i < childCount; i++) {
 			UIElement child = panel.Children[i];
 			if (child is not FrameworkElement element || GetExclude(element)) continue;
 
-			//if (panel is StackPanel) {
+			if (panel is StackPanel stackPanel) {
 				bool isLastItem = i == childCount - 1;
-				element.Margin = isLastItem ? new(0) : new(0, 0, horizontal, vertical);
-			//} else if (panel is UniformGrid uniformGrid) {
-			//	int columns = Math.Min(uniformGrid.Columns, childCount),
-			//		rows = Math.
-			//}
+				element.Margin = isLastItem ? new(0) :
+					stackPanel.Orientation == Orientation.Horizontal ? new(0, 0, horizontal, 0) : new(0, 0, 0, vertical);
+			} else if (panel is UniformGrid uniformGrid) {
+				(int columns, int rows) = GetUniformGridSize(uniformGrid);
+				int column = i % columns, row = i / columns;
+				element.Margin = new(
+					horizontal / columns * column,
+					vertical / rows * row,
+					horizontal / columns * (columns - 1 - column),
+					vertical / rows * (rows - 1 - row)
+				);
+			} else
+				throw new NotImplementedException($"Spacing does not support panel type `{panel.GetType().Name}` currently");
 		}
 	}
 }
