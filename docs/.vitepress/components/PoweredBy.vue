f15eb5bbd20b092e5a9518fd543c628906496b0f
Rantetsu Inori <shuishenme@outlook.com>
2026-07-09 03:07:48 +0800

    fix: missing icons in powered by

--- docs/.vitepress/components/PoweredBy.vue
+++ docs/.vitepress/components/PoweredBy.vue
@@ -2,9 +2,15 @@
 import { VPTeamPage, VPTeamPageTitle, VPLink } from "vitepress/theme-without-fonts";
 import { useData } from "vitepress";
 import useI18n from "@vp/use-i18n";
+import { reactive } from "vue";
 const t = useI18n();
 const title = t({ en: "The Documentation is Powered By", zh: "文档搭载于" });
 const vitePressLink = t({ en: "https://vitepress.dev/", zh: "https://vitepress.dev/zh/" });
+const links = reactive([
+	{ name: "VitePress", icon: "vitepress", href: vitePressLink },
+	{ name: "Read the Docs", icon: "readthedocs", href: "https://app.readthedocs.org/projects/otomadhelper/" },
+	{ name: "GitHub Pages", icon: "github", href: "https://github.com/otomad/OtomadHelper/tree/docs" },
+]);
 </script>
 
 <template>
@@ -13,12 +19,8 @@ const vitePressLink = t({ en: "https://vitepress.dev/", zh: "https://vitepress.d
 			<template #title>{{ title }}</template>
 		</VPTeamPageTitle>
 		<div class="container">
-			<VPLink :href="vitePressLink"><SocialIcon icon="vitepress" />VitePress</VPLink>
-			<VPLink href="https://app.readthedocs.org/projects/otomadhelper/">
-				<SocialIcon icon="readthedocs" />Read the Docs
-			</VPLink>
-			<VPLink href="https://github.com/otomad/OtomadHelper/tree/docs">
-				<SocialIcon icon="github" />GitHub Pages
+			<VPLink v-for="link in links" class="vp-external-link-icon" :href="link.href">
+				<SocialIcon :icon="link.icon" />{{ link.name }}
 			</VPLink>
 		</div>
 	</VPTeamPage>
