3c04d655cae4ec662ce5725a3138290b80cd7fc5
Rantetsu Inori <shuishenme@outlook.com>
2026-07-09 02:51:06 +0800

    fix: lightningCSS why are you not supporting it?

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -72,6 +72,10 @@ export default defineConfig({
 		},
 		build: {
 			assetsInlineLimit: 200,
+			cssMinify: "esbuild",
+			// LightningCSS doesn't support "range syntax" in container style queries now.
+			// `@container style(--outline-depth < 6)`
+			// See: https://github.com/parcel-bundler/lightningcss/issues/1069
 		},
 	},
 	lastUpdated: true,
